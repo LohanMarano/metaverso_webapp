3fe0d1ef9394076253833cf289547011b7dc2105
LohanMarano <lohan.marano@gmail.com>
2023-04-08 11:40:17 -0300

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Mundo invertido',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Ilha paradisíaca',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Laboratório do Hawkins',
+  },
+];
+
+export const startingFeatures = [
+  'Encontre um mundo que combina com você e você deseja entrar',
+  'Entre no mundo lendo basmalah para estar seguro',
+  'Não há necessidade de rodeios, apenas fique no acelerador e divirta-se',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Um novo mundo',
+    subtitle:
+        'temos a atualização mais recente com o novo mundo para você tentar, não importa',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Mais realístico',
+    subtitle:
+        'Na atualização mais recente, seus olhos estão estreitos, tornando o mundo mais realista do que nunca',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'O lançamento do Metaverso torna Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 dicas para dominar facilmente a loucura do Metaverso',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Com uma plataforma, você pode explorar o mundo inteiro virtualmente',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
